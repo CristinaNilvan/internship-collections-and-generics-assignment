c5ace496463f287b17cad5b2bff00becaf55334c
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-27 16:43:47 +0300

    Add ListPrinter

---
+++ Project/Project/ListPrinter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class ListPrinter
+    {
+        public static void PrintList<T>(GenericList<T> genericList)
+        {
+            try
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.WriteLine(genericList.GetElementAt(i));
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                throw;
+            }
+        }
+    }
+}
