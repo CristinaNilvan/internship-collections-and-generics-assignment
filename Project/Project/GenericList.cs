f491c490b40819dee8d0680286a0b068c84072b7
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-27 15:08:06 +0300

    Update GenericList and Program

--- Project/Project/GenericList.cs
+++ Project/Project/GenericList.cs
@@ -9,14 +9,87 @@ namespace Project
     internal class GenericList<T>
     {
         private int _maxSize;
-        private int _currentIndex;
+        private int _listSize;
         private T[] _elements;
 
         public GenericList(int maxSize)
         {
             _maxSize = maxSize;
-            _currentIndex = 0;
+            _listSize = 0;
             _elements = new T[maxSize];
+        }
+
+        public int Count => _listSize;
+
+        public void Add(T element)
+        {
+            _elements[_listSize++] = element;
+
+            if (_listSize > _maxSize)
+                throw new InvalidOperationException("The list is full!");
+        }
+
+        public void Remove()
+        {
+            _listSize--;
+
+            if (_listSize < 0)
+                throw new InvalidOperationException("Can't have less than 0 elements!");
+        }
+
+        public T GetElementAt(int index)
+        {
+            CheckIndex(index);
+
+            return _elements[index];
+        }
+
+        public void SetElementAt(int index, T newElement)
+        {
+            CheckIndex(index);
+
+            _elements[index] = newElement;
+        }
+
+        public void SwapElements(int firstIndex, int secondIndex)
+        {
+            CheckIndex(firstIndex);
+            CheckIndex(secondIndex);
+
+            T auxElement = _elements[firstIndex];
+            _elements[firstIndex] = _elements[secondIndex];
+            _elements[secondIndex] = auxElement;
+        }
+
+        public void SwapElements(T firstElement, T secondElement)
+        {
+            int firstIndex = -1;
+            int secondIndex = -1;
+            
+            for (int i = 0; i < _listSize; i++)
+            {
+                if (_elements[i].Equals(firstElement))
+                    firstIndex = i;
+
+                if (_elements[i].Equals(secondElement))
+                    secondIndex = i;
+            }
+
+            if (firstIndex != -1 && secondIndex != -1)
+            {
+                _elements[firstIndex] = secondElement;
+                _elements[secondIndex] = firstElement;
+            }
+            else
+            {
+                throw new NoMatchException("Not valid elements!");
+            }
+        }
+
+        private void CheckIndex(int index)
+        {
+            if (index < 0 || index >= _listSize || index >= _maxSize)
+                throw new ArgumentOutOfRangeException("Not a valid index!");
         }
     }
 }
--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Project;
+
+var myList = new GenericList<Ingredient>(20);
+
+myList.Add(new Ingredient(1, "lapte", 50));
+myList.Add(new Ingredient(2, "ou", 30));
+myList.Add(new Ingredient(3, "faina", 40));
+myList.Add(new Ingredient(4, "piersica", 10));
+myList.Add(new Ingredient(5, "rosie", 20));
+
+Console.WriteLine("Initial elements: ");
+for (int i = 0; i < myList.Count; i++)
+{
+    Console.WriteLine(myList.GetElementAt(i).Name);
+}
+Console.WriteLine();
+
+Console.WriteLine("Elements after deletion of the last element: ");
+myList.Remove();
+for (int i = 0; i < myList.Count; i++)
+{
+    Console.WriteLine(myList.GetElementAt(i).Name);
+}
+Console.WriteLine();
