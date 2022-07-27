e94dcb24b0caab548393921b9f3dacd3a2847dbc
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-27 14:31:35 +0300

    Add GenericList

---
+++ Project/Project/GenericList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class GenericList<T>
+    {
+        private int _maxSize;
+        private int _currentIndex;
+        private T[] _elements;
+
+        public GenericList(int maxSize)
+        {
+            _maxSize = maxSize;
+            _currentIndex = 0;
+            _elements = new T[maxSize];
+        }
+    }
+}
