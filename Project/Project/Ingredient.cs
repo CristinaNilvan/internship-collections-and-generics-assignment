0550d4a4eb67f0078570d0aa881f2610738386b6
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-27 14:31:16 +0300

    Add Ingredient

---
+++ Project/Project/Ingredient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Ingredient
+    {
+        public Ingredient(int id, string? name, int calories)
+        {
+            Id = id;
+            Name = name;
+            Calories = calories;
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Calories { get; set; }
+
+        public virtual string GenerateIngredientCode()
+        {
+            return $"{Id}{Name[..2]}";
+        }
+    }
+}
